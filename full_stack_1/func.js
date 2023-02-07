abdc04b398fc2bacee6c3c92469e16547f5545ee
Mihails Kostjuks <kostjuk.misha@gmail.com>
2023-02-07 22:37:26 +0200

    feat: f = (x*b)^b

---
+++ full_stack_1/func.js
@@ -0,0 +1,18 @@
+let prompt = require('Prompt')
+prompt.start();
+prompt.get(['x', 'b'],
+    function (err, result) {
+    let final_result = calculate(result.x, result.b)
+    console.log(`x = ${result.x}, b = ${result.b}, Result = ${final_result}`)
+    });
+
+function calculate(x,b) {
+    result = x * b;
+    for (i = 0; i < b-1; i++) {
+        result = result * x * b;
+    }
+    if (b == 0) {
+        result = 1
+    }
+    return result;
+}
