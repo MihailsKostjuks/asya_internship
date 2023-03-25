86e071559febd3571b0ac6074b60948ba6aa7145
MihailsKostjuks <124712859+MihailsKostjuks@users.noreply.github.com>
2023-03-25 20:50:35 +0200

    Update ParentButton.py

--- design_patterns_3/session_3_factory_observer_template/views/components/ParentButton.py
+++ design_patterns_3/session_3_factory_observer_template/views/components/ParentButton.py
@@ -1,1 +1,38 @@
+from typing import List
 
+import pygame
+pygame.init()
+
+class ParentButton:
+    def __init__(self, button_rect: pygame.Rect):
+        self.button_rect = button_rect
+        self.button_up = pygame.Surface((self.button_rect.width, self.button_rect.height), pygame.SRCALPHA)  # caurspidigs
+
+        pygame.draw.rect(
+            surface=self.button_up,
+            color=(0,0,0),
+            rect=pygame.Rect(0,0, self.button_rect.width, self.button_rect.height)
+        )
+        self.listener_click: List[callable] = []  # list of functions
+
+    def draw(self, surface: pygame.Surface):
+        surface.blit(
+            source=self.button_up,
+            dest=(self.button_rect.x, self.button_rect.y)
+        )
+
+    # triggers any time the user clicks on the map (is called in WindowMain)
+    def trigger_mouse(self, mouse_position, mouse_button_state):
+        if any(mouse_button_state):  # checks if mouse position coincides with button position
+            if self.button_rect.x < mouse_position[0] < self.button_rect.x + self.button_rect.width:
+                if self.button_rect.y < mouse_position[1] < self.button_rect.y + self.button_rect.height:
+                    for listener in self.listener_click:
+                        listener()
+
+    def add_listener_click(self, event_listener):
+        if event_listener not in self.listener_click:
+            self.listener_click.append(event_listener)
+
+    def remove_listener_click(self, event_listener):
+        if event_listener in self.listener_click:
+            self.listener_click.remove(event_listener)
