44462d0304006026cd00cb801d8259a3fdcd63b0
Mihails Kostjuks <kostjuk.misha@gmail.com>
2023-04-01 13:55:28 +0300

    feat: CWB inherits from CB

--- design_patterns_3/session_3_factory_observer_template/views/components/ComponentButton.py
+++ design_patterns_3/session_3_factory_observer_template/views/components/ComponentButton.py
@@ -1,24 +1,29 @@
 from typing import List
 
 import pygame
-
-from design_patterns_3.session_3_factory_observer_template.views.components.ParentButton import ParentButton
-
 pygame.init()
 
-class ComponentButton(ParentButton):
+class ComponentButton:
     def __init__(self, button_rect: pygame.Rect, button_text: str):
-        super().__init__(button_rect) # inheriting everything from ParentButton but adding additionally textbutton
+        self.button_rect = button_rect
         self.button_text = button_text
 
         font = pygame.font.Font('freesansbold.ttf', 23)
         self.text = font.render(button_text, True, (0,0,0), (255,255,255))
 
+        self.button_up = pygame.Surface((self.button_rect.width, self.button_rect.height), pygame.SRCALPHA)  # caurspidigs
+
+        pygame.draw.rect(
+            surface=self.button_up,
+            color=(0,0,0),
+            rect=pygame.Rect(0,0, self.button_rect.width, self.button_rect.height)
+        )
         pygame.draw.rect(
             surface=self.button_up,
             color=(255, 255, 255),
             rect=pygame.Rect(2, 2, self.button_rect.width-4, self.button_rect.height-4)
         )
+
         # rectangle with text
         self.text_rect = self.text.get_rect()
         self.text_rect.center = (
@@ -26,10 +31,33 @@ class ComponentButton(ParentButton):
             self.button_rect.height/2 + self.button_rect.y
         )
 
+
+
+        self.listener_click: List[callable] = []  # list of functions
+
     def draw(self, surface: pygame.Surface):
-        super().draw(surface)
+        surface.blit(
+            source=self.button_up,
+            dest=(self.button_rect.x, self.button_rect.y)
+        )
         if self.button_up.get_rect().collidepoint(pygame.mouse.get_pos()):
             surface.blit(
                 source=self.text,
                 dest=self.text_rect
             )
+
+    # triggers any time the user clicks on the map (is called in WindowMain)
+    def trigger_mouse(self, mouse_position, mouse_button_state):
+        if any(mouse_button_state):  # checks if mouse position coincides with button position
+            if self.button_rect.x < mouse_position[0] < self.button_rect.x + self.button_rect.width:
+                if self.button_rect.y < mouse_position[1] < self.button_rect.y + self.button_rect.height:
+                    for listener in self.listener_click:
+                        listener()
+
+    def add_listener_click(self, event_listener):
+        if event_listener not in self.listener_click:
+            self.listener_click.append(event_listener)
+
+    def remove_listener_click(self, event_listener):
+        if event_listener in self.listener_click:
+            self.listener_click.remove(event_listener)
