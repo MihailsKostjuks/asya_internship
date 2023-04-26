7f802b0e0471aacfc25d79855a0fba604e9850bb
Mihails Kostjuks <kostjuk.misha@gmail.com>
2023-04-26 15:04:11 +0300

    feat: errors fixed

--- full_stack_2/session_2_express_template/src/main.ts
+++ full_stack_2/session_2_express_template/src/main.ts
@@ -10,19 +10,19 @@ import { promises } from "fs";
 const bodyParser = require('body-parser');
 
 
-function calculateAverage (records: any[], property: string): number {
+function calculateAverage (records, property): number {
     let total: number = records.reduce((acc, cur) => acc + cur[property], 0);
     let average: number = total / records.length;
     let averageRounded: string = average.toFixed(2);
     return parseFloat(averageRounded);
 }
 
-function calculateMinimum (records: any[], property: string): number {
+function calculateMinimum (records, property): number {
     let numbers: number[] = records.map(record => record[property]);
     return Math.min(...numbers);
 }
 
-function calculateMaximum (records: any[], property: string): number {
+function calculateMaximum (records, property): number {
     let numbers: number[] = records.map(record => record[property]);
     return Math.max(...numbers);
 }
@@ -50,11 +50,31 @@ app.post('/upload', upload.single('iris'), async (req, res) => {
 
     // KEY: description
     let reqBodyString: string;
-    let reqBodyJSON : ApiRequest;
+    let reqBodyJSON : ApiRequest = {
+        species: null
+    };
 
     let filePath: string; // for write/read file
     let fileExists: boolean;
-    let response: ApiResponse;  // for calculation
+    let response: ApiResponse = { // for calculation
+
+        sepalLengthAvg: 0,
+        sepalWidthAvg: 0,
+        petalLengthAvg: 0,
+        petalWidthAvg: 0,
+
+        sepalLengthMin: 0,
+        sepalWidthMin: 0,
+        petalLengthMin: 0,
+        petalWidthMin: 0,
+
+        sepalLengthMax: 0,
+        sepalWidthMax: 0,
+        petalLengthMax: 0,
+        petalWidthMax: 0,
+
+        isSuccess: false
+    };
     let responseString: string;
 
     if (req.body.description) {  // checks if description field was given or not
@@ -62,106 +82,62 @@ app.post('/upload', upload.single('iris'), async (req, res) => {
         reqBodyJSON = JSON.parse(reqBodyString);
     }
 
-    let rawIrisRecords: string = fs.readFileSync(
+    let rawIrisDataset: string = fs.readFileSync(
         './files/requests/iris.json',
         {encoding: "utf-8"}
     )
-    let irisRecords = JSON.parse(rawIrisRecords); // string -> json
+
+    let irisDataset = JSON.parse(rawIrisDataset); // string -> json
 
     try {
-        if (reqBodyJSON !== undefined) {
+        // if description field was given:
+        if (reqBodyJSON.species) { // if null then returns false, if was given in Postman then --> true
             if (species.includes(reqBodyJSON.species)) {
-
                 filePath = `files/responses/calculated_dataset_${reqBodyJSON.species}.json`;
                 fileExists = fs.existsSync(filePath);
-
-                if (!fileExists) {  // if doesn't exist
-
-                    const irisRecordsPerSpecies = irisRecords.filter((irisRecord) => {
-                        return irisRecord.species === reqBodyJSON.species;
-                    });
-
-                    response = {
-
-                        sepalLengthAvg: calculateAverage(irisRecordsPerSpecies, 'sepalLength'),
-                        sepalWidthAvg: calculateAverage(irisRecordsPerSpecies, 'sepalWidth'),
-                        petalLengthAvg: calculateAverage(irisRecordsPerSpecies, 'petalLength'),
-                        petalWidthAvg: calculateAverage(irisRecordsPerSpecies, 'petalWidth'),
-
-                        sepalLengthMin: calculateMinimum(irisRecordsPerSpecies, 'sepalLength'),
-                        sepalWidthMin: calculateMinimum(irisRecordsPerSpecies, 'sepalWidth'),
-                        petalLengthMin: calculateMinimum(irisRecordsPerSpecies, 'petalLength'),
-                        petalWidthMin: calculateMinimum(irisRecordsPerSpecies, 'petalWidth'),
-
-                        sepalLengthMax: calculateMaximum(irisRecordsPerSpecies, 'sepalLength'),
-                        sepalWidthMax: calculateMaximum(irisRecordsPerSpecies, 'sepalWidth'),
-                        petalLengthMax: calculateMaximum(irisRecordsPerSpecies, 'petalLength'),
-                        petalWidthMax: calculateMaximum(irisRecordsPerSpecies, 'petalWidth'),
-
-                        isSuccess: true
-                    };
-
-                    await promises.writeFile(  // fs needs a callback
-                        filePath,
-                        JSON.stringify(response // pirms norakstisanas vajag json -> string
-                        )
-                    )
-                }
-
-                responseString = await promises.readFile( // variable that contains results' content
-                    filePath, // promises -> no callback
-                    {encoding: "utf-8"}
-                )
-                let finalResponse: ApiResponse = JSON.parse(responseString);
-                res.send(finalResponse); // looks like a structured json and not like a string
-
-            }
-            else {  // description was given but the species is invalid
+                irisDataset = irisDataset.filter((irisRecord) => {
+                    return irisRecord.species === reqBodyJSON.species;
+                });
+            } else {  // description was given but the species is invalid
                 res.send("There's no such species found.");
             }
         }
-
-        else if (reqBodyJSON === undefined) {  // if description fields was not given at all
-
+        else {  // if description fields was not given at all
             filePath = `files/responses/calculated_dataset.json`;
             fileExists = fs.existsSync(filePath);
-
-            if (!fileExists) {
-
-                response = {
-                    sepalLengthAvg: calculateAverage(irisRecords, 'sepalLength'),
-                    sepalWidthAvg: calculateAverage(irisRecords, 'sepalWidth'),
-                    petalLengthAvg: calculateAverage(irisRecords, 'petalLength'),
-                    petalWidthAvg: calculateAverage(irisRecords, 'petalWidth'),
-
-                    sepalLengthMin: calculateMinimum(irisRecords, 'sepalLength'),
-                    sepalWidthMin: calculateMinimum(irisRecords, 'sepalWidth'),
-                    petalLengthMin: calculateMinimum(irisRecords, 'petalLength'),
-                    petalWidthMin: calculateMinimum(irisRecords, 'petalWidth'),
-
-                    sepalLengthMax: calculateMaximum(irisRecords, 'sepalLength'),
-                    sepalWidthMax: calculateMaximum(irisRecords, 'sepalWidth'),
-                    petalLengthMax: calculateMaximum(irisRecords, 'petalLength'),
-                    petalWidthMax: calculateMaximum(irisRecords, 'petalWidth'),
-
-                    isSuccess: true
-                };
-
-
-                await promises.writeFile(  // fs needs a callback
-                    filePath,
-                    JSON.stringify(response // pirms norakstisanas vajag json -> string
-                    )
-                )
-            }
-
-            responseString = await promises.readFile( // variable that contains results' content
-                filePath,
-                {encoding: "utf-8"}
-            )
-            let finalResponse: ApiResponse = JSON.parse(responseString); // looks like a structured json and not like a string
-            res.send(finalResponse);
         }
+        if (!fileExists) {
+            response = {
+                sepalLengthAvg: calculateAverage(irisDataset, 'sepalLength'),
+                sepalWidthAvg: calculateAverage(irisDataset, 'sepalWidth'),
+                petalLengthAvg: calculateAverage(irisDataset, 'petalLength'),
+                petalWidthAvg: calculateAverage(irisDataset, 'petalWidth'),
+
+                sepalLengthMin: calculateMinimum(irisDataset, 'sepalLength'),
+                sepalWidthMin: calculateMinimum(irisDataset, 'sepalWidth'),
+                petalLengthMin: calculateMinimum(irisDataset, 'petalLength'),
+                petalWidthMin: calculateMinimum(irisDataset, 'petalWidth'),
+
+                sepalLengthMax: calculateMaximum(irisDataset, 'sepalLength'),
+                sepalWidthMax: calculateMaximum(irisDataset, 'sepalWidth'),
+                petalLengthMax: calculateMaximum(irisDataset, 'petalLength'),
+                petalWidthMax: calculateMaximum(irisDataset, 'petalWidth'),
+
+                isSuccess: true
+            };
+
+            await promises.writeFile(  // fs needs a callback
+                filePath,
+                JSON.stringify(response) // pirms norakstisanas vajag json -> string
+            )
+        }
+
+        responseString = await promises.readFile( // variable that contains results' content
+            filePath, // promises -> no callback
+            {encoding: "utf-8"}
+        )
+        let finalResponse: ApiResponse = JSON.parse(responseString);
+        res.send(finalResponse); // looks like a structured json and not like a string
     }
     catch (error){
         console.log(error);  // vareju ari send isSuccess: false bet tad vajag visas avg min max darit ?(optional)
@@ -175,7 +151,6 @@ app.get('/404', (req, res) => {
     res.send('Something went wrong.')
 
 })
-
 
 app.listen(
     8000,
